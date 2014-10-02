1a902eb5880dd61dc085937e4fd3c347ed1cb665
Parker-Stevens <elvenhawk2014@aol.com>
2014-10-02 16:33:24 -0400

    Alignment fixes

--- StevensP/Minecraft/src/VMC/stevens/parker/Main.java
+++ StevensP/Minecraft/src/VMC/stevens/parker/Main.java
@@ -1,12 +1,11 @@
 package VMC.stevens.parker;
 
-public class Main {
-
-	public static void main(String[] args) {
+public class Main
+{
+	public static void main(String[] args)
+	{
 		// TODO Auto-generated method stub
-		//Test comment for Parker.
-		//This worked.
-
+		// Test comment for Parker.
+		// This worked.
 	}
-
 }
